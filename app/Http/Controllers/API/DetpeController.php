519d23c2c56698bdfe3353d7ad8b6f36b264962d
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-08-04 22:17:46 -0500

    fixed mprecio calculation

--- app/Http/Controllers/API/DetpeController.php
+++ app/Http/Controllers/API/DetpeController.php
@@ -140,12 +140,13 @@ class DetpeController extends Controller
 
         $detpe = Detpe::find($detpe_id);
 
-        $mprecio = $new_detpe['MCODDFA'] == 'Precio especial' ? $new_detpe['MPRECIO'] * $igv : $new_detpe['MPRECIO'];
+        $mprecio = $new_detpe['MPRECIO'] * $igv;
         $mpordct1 = !is_null($famdfa) ? $famdfa['MPOR_DFA'] : 0.000;
         $mvalven = round($mprecio * $new_detpe['MCANTIDAD'], 2);
         $mdcto = round($mvalven * ($mpordct1 / 100), 2);
         $migv = round(($mvalven - $mdcto) - (($mvalven - $mdcto) / $igv), 2);
 
+        $new_detpe['MPRECIO'] = $mprecio;
         $new_detpe['MPORDCT1'] = $mpordct1;
         $new_detpe['MVALVEN'] = $mvalven;
         $new_detpe['MDCTO'] = $mdcto;
