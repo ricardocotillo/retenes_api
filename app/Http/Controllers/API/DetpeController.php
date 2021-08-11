dcb4e443e0d6c50c99bd80676c0bece1fa17f21b
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-08-11 16:37:27 -0500

    fixed update

--- app/Http/Controllers/API/DetpeController.php
+++ app/Http/Controllers/API/DetpeController.php
@@ -152,7 +152,7 @@ class DetpeController extends Controller
         $new_detpe['MDCTO'] = $mdcto;
         $new_detpe['MIGV'] = $migv;
         $new_detpe['MINDOBSQ'] = $new_detpe['MCODDFA'] == 'Bono' ? 'D' : 'N';
-        $new_detpe['estado'] = $detpe->cabpe->estado == 'terminado' ? 1 : 0;
+        $new_detpe['estado'] = 1;
 
         $detpe->fill($new_detpe);
         $detpe->save();
