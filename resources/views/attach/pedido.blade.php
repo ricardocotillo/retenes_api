b6e29a173b5b0d58e7dbb2945cf4690fb44ed609
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-09-13 11:29:30 -0500

    fixed currency

--- resources/views/attach/pedido.blade.php
+++ resources/views/attach/pedido.blade.php
@@ -226,9 +226,16 @@
     			</td>
     			<td style="border-top: 2px solid #000000; border-bottom: 2px solid #000000; border-left: 2px solid #000000; border-right: 2px solid #000000" colspan="3" align="right" valign=bottom>
     				<b>
-    					<font size=3>$   {{number_format(array_reduce($value, function($carry, $p) {
-    					    return $carry + ($p['MCODDFA'] != 'Bono' ? $p['MVALVEN'] - $p['MDCTO'] : 0);
-    					  }), 2, '.', '')}}</font>
+    					<font size=3>
+							@if ($flavor == 'filtros')
+								S/  
+							@else
+								$  
+							@endif
+							{{number_format(array_reduce($value, function($carry, $p) {
+								return $carry + ($p['MCODDFA'] != 'Bono' ? $p['MVALVEN'] - $p['MDCTO'] : 0);
+							}), 2, '.', '')}}
+						</font>
     				</b>
     			</td>
     		</tr>
