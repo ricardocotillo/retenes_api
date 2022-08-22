171fec078598311aa2200aee8ce7240ef2104fd8
ricardocotillo <ricardo.cotillo@gmail.com>
2022-08-22 16:51:53 -0500

    added optimize route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/optimize', function () {
+    \Artisan::call('optimize');
+    return response()->json(['optimize' => 'done'], 200);
+});
