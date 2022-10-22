54d9cb8f3fbd1c1742abd574e63a271c9b2bf8d3
ricardocotillo <ricardo.cotillo@gmail.com>
2022-10-22 10:26:16 -0500

    added new model and migrations

---
+++ app/Http/Controllers/API/InputController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Input;
+use Illuminate\Http\Request;
+
+class InputController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Input  $input
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Input $input)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Input  $input
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Input $input)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Input  $input
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Input $input)
+    {
+        //
+    }
+}
