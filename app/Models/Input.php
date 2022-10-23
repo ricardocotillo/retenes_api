f3dcdcddf4c4b49b8daae9598cd5da07ea2d2f27
ricardocotillo <ricardo.cotillo@gmail.com>
2022-10-23 17:04:26 -0500

    added relationship

--- app/Models/Input.php
+++ app/Models/Input.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Option;
 
 class Input extends Model
 {
     use HasFactory;
+
+    public function options() {
+        return $this->hasMany(Option::class);
+    }
 }
