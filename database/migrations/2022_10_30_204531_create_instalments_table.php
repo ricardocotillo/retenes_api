35f5dfd6d247d688f33ba67fca61081983c0e362
ricardocotillo <ricardo.cotillo@gmail.com>
2022-10-30 15:51:01 -0500

    added instalment model

---
+++ database/migrations/2022_10_30_204531_create_instalments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstalmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instalments', function (Blueprint $table) {
+            $table->id();
+            $table->string('mnserie', 4);
+            $table->string('mnroped', 7);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instalments');
+    }
+}
