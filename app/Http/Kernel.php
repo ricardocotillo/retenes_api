6c3974b8486e2a10ada59dbeb36dce4395349e28
ricardocotillo <ricardo.cotillo@gmail.com>
2023-12-15 18:18:05 -0500

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\Cors::class,
         \Illuminate\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
