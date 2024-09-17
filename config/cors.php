f616c80e1d750d25003438fd28aeb85bc459c12b
ricardocotillo <ricardo.cotillo@gmail.com>
2024-09-17 07:25:56 -0500

    allow origins

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', '*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://willybusch.com.pe', 'https://filtroswillybusch.com.pe', '*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
