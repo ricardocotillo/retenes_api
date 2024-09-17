81e50b115cbdefedfa73b06c74c6e90f5f5a97bc
ricardocotillo <ricardo.cotillo@gmail.com>
2024-09-17 08:37:35 -0500

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', '*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://willybusch.com.pe', 'https://filtroswillybusch.com.pe', '*'],
     'allowed_origins_patterns' => [],
