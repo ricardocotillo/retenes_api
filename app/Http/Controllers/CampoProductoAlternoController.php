bd1f90e82c134e1792c4630f0d902f29364f8965
ricardocotillo <ricardo.cotillo@gmail.com>
2025-05-28 13:48:05 -0500

    feat: add CampoProductoAlterno model, controller and migration for alternate product fields

---
+++ app/Http/Controllers/CampoProductoAlternoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CampoProductoAlterno;
+use Illuminate\Http\Request;
+
+class CampoProductoAlternoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CampoProductoAlterno  $campoProductoAlterno
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CampoProductoAlterno $campoProductoAlterno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CampoProductoAlterno  $campoProductoAlterno
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CampoProductoAlterno $campoProductoAlterno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CampoProductoAlterno  $campoProductoAlterno
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CampoProductoAlterno $campoProductoAlterno)
+    {
+        //
+    }
+}
