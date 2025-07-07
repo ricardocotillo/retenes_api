808b82d9fd9dab983bb97f78b8eb452794821f55
ricardocotillo <ricardo.cotillo@gmail.com>
2025-07-07 11:41:31 -0500

    feat: update Filtros Willy Busch logo path and uncomment logo display code

--- resources/views/attach/pedido.blade.php
+++ resources/views/attach/pedido.blade.php
@@ -46,7 +46,7 @@
 		<td colspan="2" rowspan=6 height="10" align="center" valign=bottom>
 			@if ($flavor == 'filtros')
 				<div></div>
-				{{-- <img src="{{ 'data:image/jpeg;base64,'.base64_encode(file_get_contents('http://filtroswillybusch.com.pe/aplicativo/img/wb.jpeg')) }}" width="100" height="100" id="logo"> --}}
+				<img src="{{ 'data:image/jpeg;base64,'.base64_encode(file_get_contents('http://filtroswillybusch.com.pe/logo/filtros_logo.png')) }}" width="100" height="100" id="logo">
 			@else
 				<img src="{{ 'data:image/jpeg;base64,'.base64_encode(file_get_contents('http://willybusch.com.pe/logos/retenes_logo.png')) }}" width="100" height="100" id="logo">
 			@endif
