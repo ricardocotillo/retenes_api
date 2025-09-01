2940049e39f0d97915c70b7bc2954dcc13c04b17
ricardocotillo <ricardo.cotillo@gmail.com>
2025-09-01 12:25:23 -0500

    feat: add mincred and mcla_prod nullable columns to articulo_famdfa table

---
+++ database/migrations/2025_08_29_193149_mindcred_mcla_prod_mniv_desc.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articulo_famdfa', function (Blueprint $table) {
+            $table->string('mincred', 2)->nullable();
+            $table->string('mcla_prod', 3)->nullable();
+            // $table->integer('mniv_desc'); # 1 = retenes, 2 = repuestos, 3 = general
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articulo_famdfa', function (Blueprint $table) {
+            //
+        });
+    }
+};
