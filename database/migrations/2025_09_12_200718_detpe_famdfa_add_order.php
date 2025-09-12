6bf23e94f902a3ee05322164bf48679ae540b2c2
ricardocotillo <ricardo.cotillo@gmail.com>
2025-09-12 15:09:23 -0500

    feat: add virtual order column to detpe_famdfa table based on type and mcla_prod fields

---
+++ database/migrations/2025_09_12_200718_detpe_famdfa_add_order.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detpe_famdfa', function (Blueprint $table) {
+            $table->integer('order')->virtualAs(DB::raw("CASE WHEN type = 'item' THEN 1 WHEN type = 'retenes' AND mcla_prod IS NOT NULL THEN 2 WHEN type = 'retenes' AND mcla_prod IS NULL THEN 3 ELSE 4 END"));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detpe_famdfa', function (Blueprint $table) {
+            //
+        });
+    }
+};
