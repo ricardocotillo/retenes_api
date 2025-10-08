6c91ebafb296197adb6b0b4300fa8b4f18e5e08c
ricardocotillo <ricardo.cotillo@gmail.com>
2025-10-08 16:33:40 -0500

    feat: add ListaPrecioController import to api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ListaPrecioController;
 use App\Http\Controllers\API\ArticuloController;
 use App\Http\Controllers\API\CcmcliController;
 use App\Http\Controllers\API\CcmcpaController;
