41af9429809ef8609182e0db0e12468e74a1c54d
ricardocotillo <ricardo.cotillo@gmail.com>
2026-02-03 23:30:09 +0000

    feat: add SettingController with show endpoint for retrieving settings
    - Created new SettingController with resource methods
    - Implemented show method to return setting as JSON response
    - Left index, store, update, and destroy methods as stubs for future implementation

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Setting $setting)
+    {
+        return response()->json($setting);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Setting $setting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Setting $setting)
+    {
+        //
+    }
+}
