703f07cb43db4a050725a7604091053319897d52
ricardocotillo <ricardo.cotillo@gmail.com>
2026-02-07 22:06:02 +0000

    chore: normalize indentation in new_pedido template head section
    - Changed head section indentation from 2 spaces to tabs
    - Maintains consistency with project formatting standards

--- resources/views/new_pedido.blade.php
+++ resources/views/new_pedido.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
 </head>
 <body style="font-family: 'Open Sans', sans-serif;">
   <h1 style="color: #000000; font-size: 10px; margin-bottom: 0;">COD. VENDEDOR: 0001</h1>
