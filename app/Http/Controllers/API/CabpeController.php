8e15401adeff0a88a77a379141b7365379a71b8b
ricardocotillo <ricardo.cotillo@gmail.com>
2026-02-18 13:00:26 -0500

    refactor: update PDF view reference from pedido to new_pedido in CabpeController
    - Changed PDF::loadView() to reference 'attach.new_pedido' instead of 'attach.pedido'

--- app/Http/Controllers/API/CabpeController.php
+++ app/Http/Controllers/API/CabpeController.php
@@ -655,7 +655,7 @@ class CabpeController extends Controller
             $info = $this->get_pedido_info($cabpes);
         }
         PDF::setOptions(['unicode' => true]);
-        $document = PDF::loadView('attach.pedido', $info);
+        $document = PDF::loadView('attach.new_pedido', $info);
         return $download ? $document->download('pedido.pdf') : $document->output();
     }
 
