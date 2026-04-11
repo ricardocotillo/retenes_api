2cdacae32ed43eb1ac53c0a199b211768705bec7
ricardocotillo <ricardo.cotillo@gmail.com>
2026-04-11 09:36:39 -0500

    refactor: adjust DESCRIPCIÓN and PRECIO U. NETO column widths in new_pedido template
    - Increased DESCRIPCIÓN column width from 91px to 93px
    - Decreased PRECIO U. NETO column width from 42px to 40px

--- resources/views/attach/new_pedido.blade.php
+++ resources/views/attach/new_pedido.blade.php
@@ -232,11 +232,11 @@
                         <tr>
                             <th class="pc" style="width: 48px;">CÓDIGO</th>
                             <th class="pc" style="width: 32px;">CANTIDAD</th>
-                            <th class="pc" style="width: 91px;">DESCRIPCIÓN</th>
+                            <th class="pc" style="width: 93px;">DESCRIPCIÓN</th>
                             <th class="pc" style="width: 38px;">INT-EXT-ALT</th>
                             <th class="pc" style="width: 42px;">ESTADO</th>
                             <th class="pc" style="width: 34px;">PRECIO U.</th>
-                            <th class="pc" style="width: 42px;">PRECIO U. NETO</th>
+                            <th class="pc" style="width: 40px;">PRECIO U. NETO</th>
                             <th class="pc" style="width: 42px;">DESCUENTO</th>
                             <th class="pc" style="width: 34px;">TOTAL</th>
                         </tr>
