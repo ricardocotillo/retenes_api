e9bd8d4000a448a147ee983662e9ba88389a793b
ricardocotillo <ricardo.cotillo@gmail.com>
2026-04-15 09:16:59 -0500

    refactor: remove tax division from unit net price calculation in new_pedido template
    - Removed division by 1.18 from precio_neto per unit calculation in product table row
    - Changed from `($v['precio_neto'] / $v['MCANTIDAD']) / 1.18` to `$v['precio_neto'] / $v['MCANTIDAD']`

--- resources/views/attach/new_pedido.blade.php
+++ resources/views/attach/new_pedido.blade.php
@@ -259,7 +259,7 @@
                                     <td class="pc-c">{{ $v['articulo']['MDIM_INT1'] ?? '0' }}-{{ $v['articulo']['MDIM_EXT1'] ?? '0' }}-{{ $v['articulo']['MDIM_ALT1'] ?? '0' }}</td>
                                     <td class="pc-c text-first-upper">{{ $v['item_state'] }}</td>
                                     <td class="pc-c">{{ number_format($v['MPRECIO'], 2, '.', '') }}</td>
-                                    <td class="pc-c">{{ number_format(($v['precio_neto'] / $v['MCANTIDAD']) / 1.18, 2, '.', '') }}</td>
+                                    <td class="pc-c">{{ number_format($v['precio_neto'] / $v['MCANTIDAD'], 2, '.', '') }}</td>
                                     <td class="pc-c">{{ $v['descrip'] }}</td>
                                     <td class="pc-c">{{ number_format($v['precio_neto'], 2, '.', '') }}</td>
                                 </tr>
